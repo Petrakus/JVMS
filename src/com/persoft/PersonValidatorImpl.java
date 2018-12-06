69eba4f72a97e41f4abc3105a3dffb1e4d196d13
Petar Petrov <p.petrov@ibs.bg>
2018-12-06 12:03:33 +0200

    lessThan and lessThanOrEqual methods added.

--- src/com/persoft/PersonValidatorImpl.java
+++ src/com/persoft/PersonValidatorImpl.java
@@ -6,15 +6,18 @@ public class PersonValidatorImpl implements PersonValidator {
     @Override
     public void validate(Person person) {
 
-        ValidatorUtil.notNull
-                .and(ValidatorUtil.notEmpty)
-                .and(ValidatorUtil.maxLength(5))
+        ValidatorUtil.notNull()
+                .and(ValidatorUtil.notEmpty())
+                .and(ValidatorUtil.length(2,5))
                 .validate(person.getFirstName())
                 .throwIfInvalid("Pleace specify valid firstName!");
 
+        ValidatorUtil.notNull().and(ValidatorUtil.lessThan(9)).validate(person.getAge()).throwIfInvalid("Age is not valid");
+        ValidatorUtil.notNull().and(ValidatorUtil.lessThanOrEqual(19)).validate(person.getAge()).throwIfInvalid("Age is not valid");
+
         if (person.isAdmin())
-            ValidatorUtil.notNull
-                    .and(ValidatorUtil.notEmpty)
+            ValidatorUtil.notNull()
+                    .and(ValidatorUtil.notEmpty())
                     .and(ValidatorUtil.equalCheck("Petar"))
                     .validate(person.getFirstName())
                     .throwIfInvalid("Admin firstName is not valid.");
--- src/com/persoft/utils/ValidatorUtil.java
+++ src/com/persoft/utils/ValidatorUtil.java
@@ -9,8 +9,16 @@ public class ValidatorUtil {
 
     private ValidatorUtil() { }
 
-    public static final Validation<Object> notNull = GenericValidation.from(Objects::nonNull);
-    public static final Validation<Object> notEmpty = GenericValidation.from(s -> !((String) s).isEmpty());
+    public static Validation<Object> notNull() {
+        return GenericValidation.from(Objects::nonNull);
+    }
+
+    /**
+     * Note: Only valid on string properties
+     */
+    public static Validation<Object> notEmpty(){
+        return GenericValidation.from(s -> !((String) s).isEmpty());
+    }
 
     public static Validation<Object> equalCheck(Object value) {
         return GenericValidation.from(v -> v.equals(value));
@@ -19,16 +27,59 @@ public class ValidatorUtil {
         return GenericValidation.from(v -> !v.equals(value));
     }
 
+    /**
+     * Note: Only valid on string properties
+     */
     public static Validation<Object> maxLength(int limit) {
         return GenericValidation.from(s -> ((String) s).length() <= limit);
     }
 
+    /**
+     * Note: Only valid on string properties
+     */
     public static Validation<Object> minLength(int size) {
         return GenericValidation.from(s -> ((String) s).length() >= size);
     }
 
+    /**
+     * Note: Only valid on string properties
+     */
     public static Validation<Object> length(int moreThan, int lessThan) {
-        return maxLength(moreThan).and(minLength(lessThan));
+        return minLength(moreThan).and(maxLength(lessThan));
+    }
+
+    /**
+     * Note: Only valid for number properties
+     */
+    public static Validation<Object> lessThan(Number lessThan) {
+        return GenericValidation.from(i -> {
+            if(i instanceof Double) {
+                return (Double) i < lessThan.doubleValue();
+            } else if(i instanceof Float) {
+                return (Float) i < lessThan.floatValue();
+            } else if(i instanceof Long) {
+                return (Long) i < lessThan.longValue();
+            } else {
+                return (Integer) i < lessThan.intValue();
+            }
+        });
+    }
+
+    /**
+     * Note: Only valid for number properties
+     */
+    public static Validation<Object> lessThanOrEqual(Number lessThan) {
+        return GenericValidation.from(i -> {
+            if(i instanceof Double) {
+                return (Double) i <= lessThan.doubleValue();
+            } else if(i instanceof Float) {
+                return (Float) i <= lessThan.floatValue();
+            } else if(i instanceof Long) {
+                return (Long) i <= lessThan.longValue();
+            } else {
+                return (Integer) i <= lessThan.intValue();
+            }
+        });
     }
 
 }
